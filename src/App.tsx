dbce72976193b0c69765b20b05ad5917b1c9346c
mgasparUNC <mgaspar@unc.edu>
2023-09-09 17:56:38 -0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import './App.css'
+import { Navbar, Hero } from './components' 
+
+function App() {
+
+  return (
+  <>
+  
+  <div className="w-full px-6 flex justify-center items-center">
+    <div className="xl:max-w-[1280px] w-full">
+      <Navbar />
+    </div>
+  </div>
+
+  <div className="w-full px-6 flex justify-center items-center">
+    <div className="xl:max-w-[1280px] w-full">
+      <Hero />
+    </div>
+  </div>
+      
+  </>
+
+  )
+      
+    
+}
+
+export default App
---
+++ src/components/index.ts
@@ -0,0 +1,7 @@
+import Navbar from "./Navbar.tsx" ;
+import Hero from "./Hero.tsx" ;
+
+export{
+    Navbar,
+    Hero,
+}
