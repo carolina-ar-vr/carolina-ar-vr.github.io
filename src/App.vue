7b1f196733d7e2b85392246a2d228452c658ac5d
Yuanyao Lin <yuanyaolin13@gmail.com>
2023-10-10 21:11:28 -0400

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import NavBar from './components/NavBar.vue'
+</script>
+
+<template>
+  <!-- Navigation Bar -->
+  <div class = "navi-bar">
+    <nav-bar>
+      <a v-bind:  >
+    </nav-bar>
+  </div>
+
+  <!-- HERO -->
+  <div class = "hero">
+
+  </div>
+
+  <!--    <a href="https://vitejs.dev" target="_blank">-->
+  <!--      <img src="/vite.svg" class="logo" alt="Vite logo" />-->
+  <!--    </a>-->
+  <!--    <a href="https://vuejs.org/" target="_blank">-->
+  <!--      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />-->
+  <!--    </a>-->
+</template>
+
+<style scoped>
+.navi-bar {
+  width: 1512px;
+  height: 96px;
+  flex-shrink: 0;
+  border-radius: 6px 6px 0px 0px;
+  background: rgba(255, 255, 255, 0.20);
+  box-shadow: 0px 4px 14px 0px rgba(21, 38, 92, 0.40);
+}
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <h1>TEST</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue } from 'vue';
+import {VueRouter} from 'vue-router';
+import About from './directories/About.vue'
+import Contact from './directories/Contact.vue';
+import Discover from './directories/Discover.vue';
+import Events from './directories/Events.vue';
+import Home from './directories/Home.vue';
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: About
+  },
+  {
+    path: '/contact',
+    name: 'Contact',
+    component: Contact
+  },
+  {
+    path: '/discover',
+    name: 'Discover',
+    component: Discover
+  },
+  {
+    path: '/events',
+    name: 'Events',
+    component: Events
+  }
+]
+
+
+export default {
+  data: () => {
+    return {
+      name: 'nav-bar'
+    };
+  },
+}
+</script>
+
+<style scoped>
+  h1 {
+    color: #213547;
+  }
+</style>
