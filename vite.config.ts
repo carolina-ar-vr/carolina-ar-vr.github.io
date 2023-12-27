4d449b2abd232e0a444ca6959d5d9a54ea3ada8a
Yuanyao Lin <yuanyaolin13@gmail.com>
2023-12-27 17:15:18 -0500

    including base for website for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,11 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/carolina-ar-vr.github.io/",
   plugins: [vue()],
+  resolve: {
+    alias: [
 
+    ]
+  },
 })
