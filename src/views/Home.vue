3cf6922fecad2a4d791ce7375b2faa623fc3f573
Yuanyao Lin <yuanyaolin13@gmail.com>
2024-01-06 22:36:49 -0500

    start of hero

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,49 @@
 <template>
-  <div class = "w-screen h-screen inset-0">
-    <h1 class = "text-white">
-      HOME
-    </h1>
+  <div class = "w-screen h-screen inset-0 flex">
+    <div class = "w-1/2 flex items-center">
+      <div class = "bg-amber-400">
+        <h1 class = "">
+          CAROLINA
+        </h1>
+        <h1 class = "">
+          AR/VR
+        </h1>
+        <h3 class = "">
+          Carolina AR/VR (aka CARVR) is a student organization at UNC-Chapel Hill that promotes student
+          exploration and development of extended reality technologies!
+        </h3>
+      </div>
+      <div>
+
+      </div>
+    </div>
+    <div class = "w-1/2 bg-carvr flex items-center">
+      <img src="../assets/CARVR2D.png" alt="carvrlogo">
+    </div>
   </div>
+  <div class = "w-screen h-screen inset-0 flex">
+    <div class = "w-1/2 bg-carvr flex items-center">
+      <img src="../assets/CARVR2D.png" alt="carvrlogo">
+    </div>
+    <div class = "w-1/2 flex items-center">
+      <div class = "bg-amber-400">
+        <h1 class = "">
+          What is AR/VR
+        </h1>
+        <h3 class = "">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+          labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+          nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+          velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+          proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </h3>
+      </div>
+      <div>
+
+      </div>
+    </div>
+  </div>
+
 
 </template>
 
