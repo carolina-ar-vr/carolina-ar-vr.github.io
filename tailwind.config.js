e4df54aa47a3024e6acd91c28486aaf47780ff17
Yuanyao Lin <yuanyaolin13@gmail.com>
2024-01-17 14:18:33 -0500

    inclusion of a primative pink/purple blur stylistic changes as well

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,8 +7,11 @@ export default {
   theme: {
     extend: {
       colors: {
-        'carvr': '#7EBDE5',
-        'carvr-nav': '#99c9ea'
+        // 'carvr': '#7EBDE5',
+        'carvr': '#6EB4E3',
+        'carvr-footer': '#343166',
+        'purple-fade': '#554688',
+        'pink-fade': '#EA3381',
       },
     },
   },
