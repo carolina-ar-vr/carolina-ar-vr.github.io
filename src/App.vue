2362e11c7c455e6b1b8be1c2591bab5d62ec9101
Yuanyao Lin <yuanyaolin13@gmail.com>
2024-01-28 18:45:00 -0500

    significant changes to the home page + general clean up

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,19 @@
 <script setup lang="ts">
 import NavBar from './components/NavBar.vue'
 import Hero from './components/Hero.vue'
+import CarvrFooter from "./components/CarvrFooter.vue";
 </script>
 
 <template>
-  <div class="w-full px-1 fixed bg-carvr top-0 z-10 ">
+  <div class="w-full px-1 fixed shadow-2xl bg-carvr top-0 z-10 ">
     <div class="w-full">
       <nav-bar className = "top w-screen"></nav-bar>
     </div>
   </div>
 
   <div class = "mt-20 w-full justify-center flex z-20">
-    <div class="w-full">
-      <hero className = "w-full h-screen"></hero>
+    <div class="w-full h-screen">
+      <hero className = "w-screen h-screen"></hero>
     </div>
   </div>
 </template>
