b987549fa5eef8835cf5baeb07d2bf03c0605842
Yuanyao Lin <yuanyaolin13@gmail.com>
2024-10-15 11:20:23 -0400

    website loading error fix attempt

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import {fileURLToPath} from "node:url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(),],
   base: "./",
   resolve: {
-    alias: [
-
-    ]
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    }
   },
 })
