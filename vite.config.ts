2a0aee6a99d14fc7b0a0ace8dab6a0fd19fc6117
Yuanyao Lin <yuanyaolin13@gmail.com>
2024-10-15 16:26:21 -0400

    fix attempt for loading issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,23 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import {fileURLToPath} from "url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(),],
   base: "./",
   resolve: {
-    alias: [
-
-    ]
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+    extensions: [
+      '.js',
+      '.json',
+      '.jsx',
+      '.mjs',
+      '.ts',
+      '.tsx',
+      '.vue',
+    ],
   },
 })
